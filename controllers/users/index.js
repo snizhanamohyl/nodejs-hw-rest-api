7da5168d3baf6996dd239f780a0420baa5c4edeb
Snizhana Mohyl <snizhana.mogyl@gmail.com>
2023-06-11 18:48:35 +0300

    add: /users/login endpoint, env variables

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -1,4 +1,8 @@
 const { ctrlWrapper } = require("../../decorators");
 const register = require("./register");
+const login = require("./login");
 
-module.exports = { register: ctrlWrapper(register) };
+module.exports = {
+  register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
+};
--- routes/api/users.js
+++ routes/api/users.js
@@ -3,5 +3,6 @@ const { usersControllers } = require("../../controllers");
 const router = express.Router();
 
 router.post("/register", usersControllers.register);
+router.post("/login", usersControllers.login);
 
 module.exports = router;
