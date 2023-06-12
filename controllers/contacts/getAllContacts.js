226ce24e0c0953156495a61cb47c86d490caddd1
Snizhana Mohyl <snizhana.mogyl@gmail.com>
2023-06-12 19:09:10 +0300

    add: filter with query

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -3,12 +3,12 @@ const { Contact } = require("../../models");
 const getAllContacts = async (req, res) => {
   const { _id: ownerId } = req.user;
 
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 10, ...query } = req.query;
 
   const skip = (page - 1) * limit;
 
   const contacts = await Contact.find(
-    { owner: ownerId },
+    { owner: ownerId, ...query },
     "-createdAt -updatedAt",
     { skip, limit }
   ).populate("owner", "email");
